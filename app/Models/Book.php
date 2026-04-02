22a1b1f3d5880c9a972e62ed5e071d0eb4263c19
DiandraAndriansyah <andriansyahdevan@gmail.com>
2026-04-02 09:28:42 +0700

    Menambahkan Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class book extends Model
+class Book extends Model
 {
-    //
+    use HasFactory;
+
+    // Tambahkan kategori dan deskripsi di sini agar bisa disimpan
+   protected $fillable = ['judul', 'penulis', 'stok', 'kategori', 'deskripsi', 'cover_image'];
 }
